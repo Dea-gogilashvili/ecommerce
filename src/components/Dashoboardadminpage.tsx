a21e83236cbe123802820ac744a967331088154f
Dea-gogilashvili <d.gogilashvili83@gmail.com>
2025-01-14 23:54:28 +0400

    finishing dashboard

--- src/components/Dashoboardadminpage.tsx
+++ src/components/Dashoboardadminpage.tsx
@@ -1,7 +1,7 @@
 const Dashboardadminpage = () => {
   return (
     <div className="max-w-[1600px] bg-gray-100 flex">
-      <div className="max-w-[260px] h-[925px] max-h-[925px] shadow-lg px-10 mt-5">
+      <div className="max-w-[260px] h-[770px] max-h-[770px]  px-10 mt-5">
         <div className="flex items-center gap-4 mb-20">
           <img src="/img/Adminpagelogo.svg" alt="Admin Logo" />
           <span className="font-extrabold text-2xl">Admin</span>
@@ -56,7 +56,7 @@ const Dashboardadminpage = () => {
             </div>
 
             <img
-              className="w-full h-auto object-contain mt-14"
+              className="w-full h-auto  mt-14"
               src="/img/Adminpage1.svg"
               alt="Sales"
             />
@@ -75,7 +75,7 @@ const Dashboardadminpage = () => {
             </div>
 
             <img
-              className="w-full h-auto object-contain mt-14"
+              className="w-full h-auto  mt-14"
               src="/img/Squigglyline.svg"
               alt="Sales Graphic"
             />
@@ -100,36 +100,73 @@ const Dashboardadminpage = () => {
               <div className="w-[272px] h-[8px] bg-slate-300 rounded-md ">
                 <div className="h-2 bg-[#4078FF] w-[190px] rounded-md "></div>
               </div>
-
-
-              
-
-
-
-
-
-
-
-
             </div>
           </div>
         </div>
 
-         
-         <div className="grid-cols-2 mt-11 ">
-          <div className="max-w-[330px] max-h-[475px] flex flex-col  gap-2 bg-white rounded-lg shadow-lg p-4">
-            <span className="font-bold">Best selling</span>
-            <span className="text-sm font-medium text-gray-500">THIS MONTH</span>
 
-            <hr className="w-[328px] h-[1px] mt-8 px-24"></hr>
+
+        <div className=" grid grid-cols-2 mt-11 ">
+          <div className=" max-w-[330px] max-h-[475px] flex flex-col  gap-2 bg-white rounded-lg shadow-lg p-4">
+          <div className="w-[328px]  flex flex-col  gap-2">
+              <span className="font-bold">Best selling</span>
+              <span className="text-sm font-medium text-gray-500"> THIS MONTH</span>
+            </div>
+           
+
+            <hr className="bg-gray-700 w-[330x] h-[0.5px]"></hr>
+            
+            <div className="flex gap-4 flex-col">
+
+            <div className="flex items-center space-x-2 mt-5">
+               <span className="font-bold text-3xl">$2,400 — </span>
+               <span className="text-lg text-gray-500">Total sales</span>
+           </div>
+
+
+              <div className="flex border-[0.5px] border-solid rounded-xl border-gray-400 text-sm justify-center w-[280px]">
+               <span className="text-gray-600">Classic monochrome tees  — </span>
+               <span className="font-medium">$940 sales</span>
+              </div>
+
+              <div className="flex border-[0.5px] border-solid rounded-xl border-gray-400 text-sm justify-center w-[280px]" >
+               <span className="text-gray-600">Monochromatic wardrobe — </span>
+               <span className="font-medium">$790 sales</span>
+              </div>
+
+              <div className="flex border-[0.5px] border-solid rounded-xl border-gray-400 text-sm justify-center w-[220px]" >
+               <span className="text-gray-600">Essential neutrals  — </span>
+               <span className="font-medium">$740 sales</span>
+              </div>
+              
+              <img className="w-[96px] h-[96px] m-4" src="/img/donati.svg"/>
+            </div>
+
           </div>
 
-
-
-         </div>
+          <div className="w-[640px] h-[435px]  bg-purple-800 ">
 
 
 
+          </div>
+         
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
       </div>
     </div>
   );
